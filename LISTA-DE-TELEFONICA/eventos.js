5bb513c849d73f5be09599bc485563e30101cf83
adriellysoares <AdriellySoares>
2022-10-03 13:18:42 -0300

    add arquivos

--- LISTA-DE-TELEFONICA/eventos.js
+++ LISTA-DE-TELEFONICA/eventos.js
@@ -1,116 +1,94 @@
-const API_URL = "http://localhost:8001";
+const API_URL = 'http://localhost:777'
 
-function marcarTodos() {
-    let todos = document.querySelectorAll('[data-check="acao"]');
-
-    todos.forEach((cadaCheck) => {
-        cadaCheck.checked = true;
-    });
-}
-
-function buscarParaEditar(id) {
-
-    fetch(API_URL+'/compras/'+id)
-    .then(res => res.json())
-    .then(dados => {
-        input_editar_id.value = id;
-        input_editar_item.value = dados.item,
-        input_editar_quantidade.value = dados.quantidade;
-});
-}
-
-
-async function editar() {
+function inserir(){
     event.preventDefault();
-
-    let dados = {
-        item: input_editar_item.value,
-        quantidade: input_editar_quantidade.value,
+    let dados ={
+        nome: input_nome.value,
+        numero: parseInt(input_numero.value),
+        cidade: input_cidade.value,
     };
-
-    await fetch(API_URL+'/compras/'+input_editar_id.value, {
-        method: 'PATCH',
-        body: JSON.stringify(dados),
-        headers: {
-            'content-type': 'application/json',
-     }
-})
-        .then(res => res.json())
-        .then(() => atualizarlista());
-
-  let x = document.querySelector('[data-bs-dismiss="offcanvas"]');
-  x.dispatchEvent(new Event('click'));
-
-}
-
-function inserir() {
-    event.preventDefault();
-
-    let dados = {
-        item: input_item.value,
-        quantidade: parseInt(input_quantidade.value),
-    };
-
-    fetch(API_URL+'/compras', {
-    method: 'POST',
-    body: JSON.stringify(dados),
-    headers: {
-        'content-Type': 'application/json',
-    }
-    }) 
-
+    fetch(API_URL+ '/telefones', {
+       method: 'POST',
+       body: JSON.stringify(dados),
+       headers: {
+        'content-type':'application/json'
+       } 
+    })
     .then(resposta => resposta.json())
     .then(resposta => atualizarlista());
 
     form_add.reset();
+}
 
+async function editar(){
+    event.preventDefault();
+        let dados = {
+            nome : input_editar_nome.value,
+            numero : input_editar_numero.value,
+            cidade : input_editar_cidade.value,
+        };
+        await fetch(API_URL+'/telefones/'+input_editar_id.value,{
+            method: 'PATCH',
+            body: JSON.stringify(dados),
+            headers: {
+                'content-type': 'application/json',
+            }
+        })
+        .then(res=>res.json())
+        .then(()=>atualizarlista());
+
+        let x = document.querySelector('[data-bs-dismiss="offcanvas"]');
+        x.dispatchEvent(new Event('click'));
+}
+
+function buscarparaeditar(id) {      
+    
+    fetch(API_URL+'/telefones/'+id)
+    .then(res=>res.json())
+    .then(dados =>{
+        input_editar_id.value = id;
+        input_editar_nome.value = dados.nome;
+        input_editar_numero.value = dados.numero;
+        input_editar_cidade.value = dados.cidade;
+        });
+    }
+        
+function atualizarlista(){
+    fetch (API_URL+ '/telefones')
+    .then((resposta)=>{
+        return resposta.json();
+    })
+    .then((lista)=>{
+        tabela_telefones.innerHTML ='';
+        lista.forEach((cadaItem) => {
+            tabela_telefones.innerHTML +=`
+            <tr>
+            <td>${cadaItem.id}</td>
+            <td>${cadaItem.nome}</td>
+            <td>${cadaItem.numero}</td>
+            <td>${cadaItem.cidade}</td>
+            <td>
+            <button onclick="buscarparaeditar(${cadaItem.id})" data-bs-toggle="offcanvas" data-bs-target="#offcanvasEditar" class="btn btn-outline-warning btn-sm">
+            Editar
+            </button> 
+            <button onclick="excluir(${cadaItem.id})" class="btn btn-outline-danger">
+            Excluir
+            </button>
+            </td>
+            </tr>`;
+        });
+    })   
 }
 
 async function excluir(id){
-let resposta = confirm('Você tem certeza?');
-
-if (resposta !== true)
-    return;
-    
-  await  fetch(API_URL+'/compras/'+id,{
-    method: 'DELETE'
-    
-});
-
-atualizarlista();
-}
-
-function atualizarlista(){
-fetch(API_URL+'/compras')
-    .then(function(resposta){
-        return resposta.json();
-    })
-    .then(function(lista){
-        tabela_compras.innerHTML ='';
-        lista.forEach(function (cadaItem){
-           tabela_compras.innerHTML +=`
-           <tr>
-                <td> <input data-check="acao" type="checkbox"> </td>
-                <td>${cadaItem.id}</td>
-                <td>${cadaItem.item}</td>
-                <td>${cadaItem.quantidade}</td>
-                <td>
-                    <button onclick="buscarParaEditar(${cadaItem.id})" data-bs-toggle="offcanvas" data-bs-target="#offcanvasEditar" class="btn btn-warning btn-sm">
-
-                    Editar
-
-                    </button>
-
-                    <button onclick="excluir(${cadaItem.id})" class="btn btn-danger">
-
-                    Excluir
-
-                    </button>
-                </td>    
-            </tr>    
-           `; 
+    let resposta = confirm('Exclui não má, mas se tu quiser arrocha ai, tem certeza?')
+        if(resposta !==true){
+            return;
+        }
+        await fetch(API_URL+'/telefones/'+id,{
+            method: 'DELETE'
         });
-    })
-}
-
+        atualizarlista();
+} 
+    
 atualizarlista();
